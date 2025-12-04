d1c61bd54036b225032c03fbeb438474bb5ecbe9
Claus <cshagen@hagens.ch>
2025-12-04 18:42:17 +0100

    cleanup day 4

--- day4.kt
+++ day4.kt
@@ -1,64 +1,55 @@
 val FILENAME = "data/d4-input.txt"
 
 fun main() {
-	println ("Part 1: ${part1(FILENAME)}")	
-	println ("Part 2: ${part2(FILENAME)}")	
+	val file = java.io.File(FILENAME)
+	println ("Part 1: ${ part1 (file.readLines()) }")	
+	println ("Part 2: ${ part2 (file.readLines()) }")	
 	}
 
-fun part1(inputFile: String): Int {
-		val file = java.io.File(inputFile)
-		var result = 0
-		var matrix = mutableListOf<CharArray>()
-		file.readLines().forEach { line ->
-				matrix.add(line.toCharArray())
-		}
-		for (i in 0 ..  matrix.size-1) {
-			for (j in 0 .. matrix[i].size-1) {
-				if (matrix[i][j] == '@' && checkRoll(matrix, i, j)) {
-					result += 1
-				}
-			}
-		}
-		return result
-}
-
-fun part2(inputFile: String): Int {
-		val file = java.io.File(inputFile)
+fun part1(lines: List<String>): Int {
 		var result = 0
 		var matrix = mutableListOf<CharArray>()
 		
-		file.readLines().forEach { line ->
-				matrix.add(line.toCharArray())
-		}
-		while (true) {
-			val (row,col) = findFirstRemovableRoll(matrix)
-			if (row == -1 && col == -1) {
-				return result
-			} else {
-				matrix[row][col] = '.'
-				result += 1
+		lines.forEach { line -> matrix.add(line.toCharArray())}
+		matrix.forEachIndexed { y, row ->
+			row.forEachIndexed { x, c ->
+				if (c == '@' && accessible(matrix, y, x) ) result += 1
 			}
 		}
 		return result
 }
 
-fun checkRoll (matrix: MutableList<CharArray>, row: Int, col: Int) : Boolean {
+fun part2(lines: List<String>): Int {
+	var result = 0
+	var matrix = mutableListOf<CharArray>()
+		
+	lines.forEach { line -> matrix.add(line.toCharArray()) }
+	while (removeFirstAccessible (matrix)) {
+		result += 1
+	}
+	return result
+}
+
+fun accessible (matrix: MutableList<CharArray>, row: Int, col: Int) : Boolean {
 	var count = 0
-	for (i in row-1 .. row+1) {
-		for (j in col-1 .. col+1) {
-			if (i >= 0  && j >= 0 && i < matrix.size && j < matrix[0].size && (i != row || j != col) && matrix[i][j] == '@') count += 1
+	for (y in row-1 .. row+1) {
+		for (x in col-1 .. col+1) {
+			if (y in 0 .. matrix.size-1 && x in 0 .. matrix[0].size-1 && (y != row || x != col) && matrix[y][x] == '@') {
+				count ++
+			}
 		}
 	}
 	return (count < 4)
 }
 
-fun findFirstRemovableRoll (matrix: MutableList<CharArray>) : Pair<Int,Int> {
-	for (i in 0 ..  matrix.size-1) {
-		for (j in 0 .. matrix[0].size-1) {
-			if (matrix[i][j] == '@' && checkRoll(matrix, i, j)) {
-				return Pair(i,j)
+fun removeFirstAccessible (matrix: MutableList<CharArray>) : Boolean {
+	matrix.forEachIndexed { y, row ->
+		row.forEachIndexed { x, c ->
+			if (c == '@' && accessible(matrix, y, x)) {
+				matrix[y][x] = '.'
+				return true
 			}
 		}
 	}
-	return Pair(-1,-1)
+	return false
 }	
