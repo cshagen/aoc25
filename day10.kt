969481050069588ed23ffcae6ef4147292817a20
Claus <cshagen@hagens.ch>
2025-12-30 10:50:08 +0100

    day 11

--- day10.kt
+++ day10.kt
@@ -1,5 +1,6 @@
 val FILENAME = "data/d10-input.txt"
 val re1 = Regex("^\\[(.+)\\] (\\(.+\\)\\s)\\{(.+)\\}$")
+val cache = hashSetOf(List(0) { 0 })
 fun main() {
 	val file = java.io.File(FILENAME)
 	println ("Part 1: ${ part1 (file.readLines()) }")	
@@ -17,59 +18,24 @@ fun part1(lines: List<String>): Long {
 			buttons.add(match.groups[2]!!.value?.trim()?.split(" ")!!.map { it.slice(1..it.length-2).split(",").map { it.toInt() } })
 		}
 	}
-
 	for (case in 0 until targetLights.size) {
 		var lights = mutableSetOf(List(targetLights[case].size) { false })
 		var found = false
 		for (round in 0 until 100) {
 			if (!found) {
-			lights = applyAllButtons(lights, buttons[case], targetLights[case]).toMutableSet()
-			for (light in lights) {
-				if (light == targetLights[case]) {
-					result += round + 1
-					found = true
-					break
+				lights = applyAllButtons(lights, buttons[case], targetLights[case]).toMutableSet()
+				for (light in lights) {
+					if (light == targetLights[case]) {
+						result += round + 1
+						found = true
+						break
+					}
 				}
 			}
-		}
 		}
 	}
 	return result
 }
-
-fun part2(lines: List<String>): Long {
-		var result = 0L
-	var targetLevels = mutableListOf<List<Int>>()
-	var buttons = mutableListOf<List<List<Int>>>()
-	for (line in lines) {
-		val match = re1.find(line)
-		if (match != null) {
-			targetLevels.add (match.groups[3]!!.value?.trim()?.split(",")!!.map { it.toInt() })
-			buttons.add(match.groups[2]!!.value?.trim()?.split(" ")!!.map { it.slice(1..it.length-2).split(",").map { it.toInt() } })
-		}
-	}
-
-	for (case in 0 until targetLevels.size) {
-		println("Case $case: ")
-		var levels = mutableSetOf(List(targetLevels[case].size) { 0 })
-		var found = false
-		for (round in 0 until 100) {
-			if (!found) {
-			levels = applyAllButtons2(levels, buttons[case], targetLevels[case]).toMutableSet()
-			for (level in levels) {
-				if (level == targetLevels[case]) {
-					result += round + 1
-					found = true
-					break
-				}
-			}
-		}
-		}
-	}
-	
-	return result
-}
-
 fun applyAllButtons (lights: Set<List<Boolean>>, buttons: List<List<Int>>, target: List<Boolean>): Set<List<Boolean>> {
 	var results = mutableSetOf<List<Boolean>>()
 	var found = false
@@ -81,33 +47,10 @@ fun applyAllButtons (lights: Set<List<Boolean>>, buttons: List<List<Int>>, targe
 				if (newLights == target) {
 					found = true
 					results.add(newLights)
-					println("Found target lights!")
+					//println("Found target lights!")
 					break
 				}
 				results.add(newLights)
-			}
-		}
-		
-	}
-	return results
-	
-}
-
-fun applyAllButtons2 (levels: Set<List<Int>>, buttons: List<List<Int>>, target: List<Int>): Set<List<Int>> {
-	var results = mutableSetOf<List<Int>>()
-	var found = false
-		
-	for (l in levels) {
-		if (!found) {
-			for (b in buttons) {
-				val newLevels = applyButton2(l, b)
-				if (newLevels == target) {
-					found = true
-					results.add(newLevels)
-					println("Found target levels!")
-					break
-				}
-				results.add(newLevels)
 			}
 		}
 		
@@ -123,6 +66,68 @@ fun applyButton (lights: List<Boolean>, button: List<Int>): List<Boolean> {
 	return newLights
 }
 
+fun part2(lines: List<String>): Long {
+	var result = 0L
+	var targetLevels = mutableListOf<List<Int>>()
+	var buttons = mutableListOf<List<List<Int>>>()
+	for (line in lines) {
+		val match = re1.find(line)
+		if (match != null) {
+			targetLevels.add (match.groups[3]!!.value?.trim()?.split(",")!!.map { it.toInt() })
+			buttons.add(match.groups[2]!!.value?.trim()?.split(" ")!!.map { it.slice(1..it.length-2).split(",").map { it.toInt() } })
+		}
+	}
+	for (case in 0 until targetLevels.size) {
+		print("[$case]")
+		var levels = mutableSetOf(List(targetLevels[case].size) { 0 })
+		result += solveCase (buttons[case],targetLevels[case])
+		
+	}
+	return result
+}
+fun solveCase (buttons: List<List<Int>>, target: List<Int>) : Long {
+	var levels = hashSetOf(List(target.size) { 0 })
+	var round = 0
+	cache.clear()
+	while (true) {
+		print(".")
+		val (solved,newLevels)  = applyAllButtons2(levels, buttons, target)
+		if (solved) {
+			println("(${round.toLong()+1})")
+			levels.clear()
+			return round.toLong() + 1
+		}
+		round++
+		levels = newLevels
+	}
+}
+
+fun applyAllButtons2 (levels: HashSet<List<Int>>, buttons: List<List<Int>>, target: List<Int>): Pair<Boolean,HashSet<List<Int>>> {
+	var results = hashSetOf<List<Int>>()
+	for (l in levels) {
+		if (!cache.contains(l)) {
+			for (b in buttons) {
+				val newLevels = applyButton2(l, b)
+				if (newLevels == target) {
+					results.add(newLevels)
+					return Pair(true,results)
+				} else if (!tooHigh(newLevels,target)) {
+					results.add(newLevels)
+				}
+			}
+			cache.add(l)
+		}
+	}
+	return Pair(false,results)
+}
+fun tooHigh (levels: List<Int>, target: List<Int>) : Boolean {
+	for (i in 0 until levels.size) {
+		if (levels[i] > target[i]) {
+			return true
+		}
+	}
+	return false
+}
 fun applyButton2 (levels: List<Int>, button: List<Int>): List<Int> {
 	var newLevels = levels.toMutableList()
 	for (i in button) {
@@ -130,3 +135,15 @@ fun applyButton2 (levels: List<Int>, button: List<Int>): List<Int> {
 	}
 	return newLevels
 }
+
+
+/*
+
+ (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+	x0(n4+n5) = 3
+	x1(n1+n5) = 5 
+ 	x2(n2+n3+n4) = 4
+	x3(n0+n1+n3) = 7
+
+
+ */
--- day11.kt
+++ day11.kt
@@ -1,107 +1,70 @@
 val FILENAME = "data/d11-input.txt"
-val re = Regex("^\\[(.+)\\] (\\(.+\\)\\s)\\{(.+)\\}$")
+
 fun main() {
 	val file = java.io.File(FILENAME)
-	//println ("Part 1: ${ part1 (file.readLines()) }")	
+	println ("Part 1: ${ part1 (file.readLines()) }")	
 	println ("Part 2: ${ part2 (file.readLines()) }")	
 	}
 
-fun part1(lines: List<String>): Int {
-	var steps = mutableMapOf<String,List<String>>()
-	val start="you"
-	var nodes = mutableMapOf<String,Int>(start to 0)
-	var current = mutableListOf<List<String>>()
+var nodes = mutableMapOf<String,Node>()
+
+fun part1(lines: List<String>): Long {
+// Read the file and create graph
 	for (line in lines) {
 		val (source, next) = line.split(":")
 		val targets = next.trim().split(" ")
-		steps[source]=targets
-	}
-	current.add(mutableListOf(start,start))
-	var done = false
-	while (!done) {
-		current = repeat1(current, steps, nodes)
-		done = true
-		for (path in current) {
-			if (path[1] != "out")
-			done = false
-			break
+		if (!nodes.contains(source)) nodes[source] = Node (source, 0, targets.toMutableSet(), mutableSetOf<String>())
+		 	else nodes[source]!!.next = targets.toMutableSet()
+		for (t in targets) {
+			if (!nodes.contains(t)) nodes[t] = Node (t, 0, mutableSetOf<String>(), mutableSetOf<String>())
+			nodes[t]!!.prev.add(source)
 		}
 	}
-	return current.size
+	// Find the paths
+	return countPaths("you","out")
 }
-
-fun part2(lines: List<String>): Int {
-	var result = 0
-	var steps = mutableMapOf<String,List<String>>()
-	val start="svr"
-	var nodes = mutableMapOf<String,Int>(start to 0)
-	var current = mutableListOf<List<String>>()
+fun part2(lines: List<String>): Long {
+	// Read the file and create graph
 	for (line in lines) {
 		val (source, next) = line.split(":")
 		val targets = next.trim().split(" ")
-		steps[source]=targets
-	}
-	current.add(mutableListOf(start))
-	var done = false
-	while (!done) {
-		current = repeat2(current, steps, nodes)
-		done = true
-		for (path in current) {
-			if (path.last() != "out")
-			done = false
-			break
+		if (!nodes.contains(source)) nodes[source] = Node (source, 0, targets.toMutableSet(), mutableSetOf<String>())
+		 	else nodes[source]!!.next = targets.toMutableSet()
+		for (t in targets) {
+			if (!nodes.contains(t)) nodes[t] = Node (t, 0, mutableSetOf<String>(), mutableSetOf<String>())
+			nodes[t]!!.prev.add(source)
 		}
-		println(current.size)
 	}
-	for (path in current) {
-			if (path.contains("dac") && (path.contains("fft"))) {
-				result +=1
-			}
-		}
-	return result
+	// Find the paths
+	val count1 = countPaths("svr", "fft")
+	val count2 = countPaths("fft","dac")
+	val count3 = countPaths("dac","out")
+	return count1 * count2 * count3
 }
-
-fun repeat1 (current: List<List<String>>, steps: MutableMap<String,List<String>>, nodes: MutableMap<String,Int> ) : MutableList<List<String>> {
-	var result = mutableListOf<List<String>>()
-	current.forEach { path ->
-		var matched = false
-		steps.forEach { step ->
-			if (path.last() == step.key) {
-				for (target in step.value) {
-					result.add(mutableListOf(path[0],target))
-					matched = true
-					}
-				}
-		} 
-		if (!matched) {
-			result.add(path)
+fun countPaths (start: String, end: String) : Long {
+	var current = mutableSetOf<String>(start)
+	for (n in nodes) {
+		n.value.count = 0
+	}
+	nodes[start]!!.count = 1
+	while (current.size > 0) {
+		current = nextRound(current, end)
+	}
+	return nodes[end]!!.count.toLong()
+}
+fun nextRound (current: Set<String>, goal: String) : MutableSet<String> {
+	var result = mutableSetOf<String>()
+	current.forEach { cur ->
+		nodes[cur]!!.next.forEach { nxt ->
+			nodes[nxt]!!.count = nodes[nxt]!!.prev.sumOf { nodes[it]!!.count }
+			if (nxt != goal) result.add(nxt)
 		}
 	}
 	return result
 }
-
-fun repeat2 (current: List<List<String>>, steps: MutableMap<String,List<String>>, nodes: MutableMap<String,Int> ) : MutableList<List<String>> {
-	var result = mutableListOf<List<String>>()
-	current.forEach { path ->
-		var matched = false
-		steps.forEach { step ->
-			if (path.last() == step.key) {
-				for (target in step.value) {
-					if (path.contains(target)) {
-						println("loop")
-					} else {
-						var p=path.toMutableList()
-						p.add(target)
-						result.add(p)
-						matched = true
-					}
-				}
-			} 
-		}
-		if (!matched) {
-			result.add(path)
-		}
-	}	
-	//println ("transitioned from $current to $result")
-	return result
-}
+data class Node (
+	val id: String, 
+	var count: Int, 
+	var next: MutableSet<String>, 
+	var prev: MutableSet<String>
+)
