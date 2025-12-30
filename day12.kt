b50cbec94ff6a96a17f8a47eb9d19c1464f8d98c
Claus <cshagen@hagens.ch>
2025-12-30 17:48:28 +0100

    clean up day 12

--- day12.kt
+++ day12.kt
@@ -14,10 +14,9 @@ fun main() {
 	}
 
 fun part1(lines: List<String>): Int {
-	// var presents = mutableMapOf<Int,Present>()
 	var present = Present()
 	var regions = mutableListOf<Region>()
-	
+	// Read file
 	for (line in lines) {
 		if (line == "") {
 			presents[Pair(present.id,0)]=present
@@ -35,58 +34,34 @@ fun part1(lines: List<String>): Int {
 				val match = re_shape.find(line)
 				if (match != null) {
 					present.addLine(match.groups[1]?.value!!)
-			} else {
-				val match = re_region.find(line)
-				if (match != null) {
-					var region = Region() 
-					region.width = match.groups[1]?.value!!.toInt() 
-					region.height = match.groups[2]?.value!!.toInt() 
-					for (c in 3..8) {
-						region.counts.add(match.groups[c]?.value!!.toInt() )
+				} else {
+					val match = re_region.find(line)
+					if (match != null) {
+						var region = Region() 
+						region.width = match.groups[1]?.value!!.toInt() 
+						region.height = match.groups[2]?.value!!.toInt() 
+						for (c in 3..8) {
+							region.counts.add(match.groups[c]?.value!!.toInt() )
+						}
+						regions.add(region)
 					}
-					regions.add(region)
 				}
 			}
-			}
 		}
 	}
-	//presents.forEach {entry ->
-		//println("[${entry.key}] ${entry.value.lines}")
-	//}
-	regions.forEach { a ->
-		a.print()
-		println(a.preCheck())
-	}
+	// Solve
 	var result = 0
 	regions.forEach { a ->
-		a.print()
-		if (a.preCheck()) {
-			result += 1
-		}
+		//a.print()
+		if (a.preCheck()) result += 1
 	}
 	return result
-	for (i in 0 until 3) {
-		println ("Checking region $i")
-		if ( regions[i].search()) {
-			println ("fit")
-			result += 1
-			println("tries: $trialCounter")
-			println("comparisons: $overlapCounter")
-		} else {
-			println("no fit")
-			println("tries: $trialCounter")
-			println("comparisons: $overlapCounter")
-		}
 	}
-	return result
-}
 
 fun part2(lines: List<String>): Int {
 	var result = 0
-	
 	return result
 }
-
 
 class Present {
 	var id = 0
@@ -120,16 +95,6 @@ class Present {
 		return p
 	}
 
-	fun copy() : Present {
-		var p = Present()
-		p.id = id
-		p.width = width
-		p.height = height
-		for (row in lines) {
-			p.lines.add(row)
-		}
-		return p
-	}
 	fun summarize() : Int {
 		var result = 0
 		for (row in lines) {
@@ -150,185 +115,13 @@ class Region {
 	fun print() {
 		println ("Region - width: $width, height: $height, counts: $counts")
 	}
-
 	fun preCheck () : Boolean {
 		var sum = 0
 		for (id in 0 until counts.size) {
 			val p = presents[Pair(id,0)]!!
 			sum += counts[id]!! * p.summarize()
 		}
-		println ("area: ${width * height}, sum of presents: $sum")
 		return (sum <= width*height)
 	}
-
-	fun search() : Boolean {
-		var done = hashSetOf<Position>()
-		var tried = hashSetOf<Position>()
-		var todo = mutableListOf<Int>()
-		
-		for ( id in 0 until counts.size) {
-			for (amount in 0 until counts[id]) {
-				todo.add(id)
-			}
-		}
-		println ("Presents: ${todo.size}")
-		return searchRecursive(done, todo, tried)
-	}
-
-	fun searchRecursive (done: Set<Position>, todo: List<Int>, tried: Set<Position>) : Boolean {
-		//println(tried.size)
-		//println(done)
-		if (todo.size == 0) {
-			println("Found solution:")
-			for (pos in done) {
-				println("ID: ${pos.id}, Dir: ${pos.direction}, X: ${pos.x}, y: ${pos.y}")
-			}
-			return true
-		}
-		if (done.size == 0) println ("")
-		val id = todo.first()
-		var noGo = tried.toHashSet()
-		var startX = 0
-		var startY = 0
-		var startDir = 0
-
-		
-		if (done.size > 0) {
-			val last = done.last()
-			val lastP = presents[Pair(last.id, last.direction)]!!
-			if (last.id == id) {
-				if (last.y < height - lastP.height -1) {
-					startY = last.y +1
-					startX = last.x
-					startDir = last.direction
-				} else if (last.x < width -lastP.width -1) {
-					startX = last.x +1
-					startDir = last.direction
-				} else if (last.direction < 3) {
-					startDir = last.direction +1
-				} else {
-					startDir = last.direction
-				}
-			}
-		} 
-		
-		for (direction in startDir..3) { 													// all directions
-		if (done.size == 0) println ("[$id][$direction]")
-			val p = presents[Pair(id,direction)]!!
-			for (newX in startX..width - p.width) { 				// all x positions
-				for (newY in startX..height - p.height) { 		// all y positions
-				if (done.size == 0) print('.')
-					val pos = Position(id, direction, newX,newY)
-					if (!noGo.contains(pos)) {
-						trialCounter++
-						var overlap = false
-						for (pos1 in done) {
-							if (overlaps (pos, pos1)) {
-								overlap = true
-								noGo.add(pos)
-								break
-							} 
-						}
-						if (!overlap) {
-							noGo.add(pos)
-							if (searchRecursive (done + pos, todo.slice(1..todo.size -1), noGo)) {
-								return true
-							}	else {
-								noGo.add(pos)
-								//println(noGo.size)
-							}
-						} 
-						
-					}
-				}
-			}
-			//println(noGo.size)
-		}
-		return false
-	}
 }
-/*
-fun searchIterative (counts: List<Int>, done: Set<Position>, todo: List<Int>, tried: Set<Position>) : Boolean {
-		if (todo.size == 0) {
-			println("Found solution:")
-			for (pos in done) {
-				println("ID: ${pos.id}, Dir: ${pos.direction}, X: ${pos.x}, y: ${pos.y}")
-			}
-			return true
-		}
-		if (done.size == 0) println ("")
-		
-		
-		 
-		for (pCount in counts) {																			// all present groups
-			for (id in 0 until pCount) {																	// all presents
-				for (direction in startDir..3) { 													// all directions
-					val p = presents[Pair(id,direction)]!!
-					for (newX in 0..width - p.width) { 								// all x positions
-						for (newY in 0..height - p.height) { 						// all y positions
-							val pos = Position(id, direction, newX,newY)
-							if (!noGo.contains(pos)) {
-								trialCounter++
-								var overlap = false
-								for (pos1 in done) {
-									if (overlaps (pos, pos1)) {
-										overlap = true
-										noGo.add(pos)
-										break
-									} 
-								}
-								if (!overlap) {
-									noGo.add(pos)
-									if (searchRecursive (done + pos, todo.slice(1..todo.size -1), noGo)) {
-										return true
-									}	else {
-										noGo.add(pos)
-										//println(noGo.size)
-									}
-							} 
-						
-					}
-				}
-			}
-			//println(noGo.size)
-		}
-		return false
-	}
-	*/
-//}
 data class Position (val id: Int, val direction: Int, val x: Int, val y: Int)
-
-fun overlaps (pos1: Position, pos2: Position) : Boolean {
-
-	if (overlapCache.contains(Triple(pos1,pos2,true))) {
-		//println("cache hit")
-		return true
-	} else if (overlapCache.contains(Triple(pos1,pos2,false))) {
-		//println("cache hit")
-		return false
-	}
-	overlapCounter++
-					
-	var p1 = presents[Pair(pos1.id,pos1.direction)]!!
-	var p2 = presents[Pair(pos2.id,pos2.direction)]!!
-	
-	val yrange1 = pos1.y until pos1.y +p1.height
-	val yrange2 = pos2.y until pos2.y +p2.height
-	val yrange = Math.max(yrange1.first,yrange2.first) .. Math.min(yrange1.last,yrange2.last)
-	val xrange1 = pos1.x until pos1.x +p1.width
-	val xrange2 = pos2.x until pos2.x +p2.width
-	val xrange = Math.max(xrange1.first,xrange2.first) .. Math.min(xrange1.last,xrange2.last)
-	
-		for (row in yrange) {
-			for (col in xrange) {
-				if (p1.lines[row-pos1.y][col-pos1.x] == '#'
-					&&  p2.lines[row-pos2.y][col-pos2.x] == '#') {
-						overlapCache.add(Triple(pos1,pos2,true))
-						return true
-					}
-			}
-		}
-
-		overlapCache.add(Triple(pos1,pos2,false))
-		return false
-	}
